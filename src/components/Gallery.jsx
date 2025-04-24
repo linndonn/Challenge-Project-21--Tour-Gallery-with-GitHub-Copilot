8653ba3ab9a617afdd93513c28b80afe534292e0
LinnDonn <linndonn@usf.edu>
2025-04-24 05:30:01 -0400

    Implement Gallery component that displays list of tours

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import TourCard from './TourCard';
+import './Gallery.css'; // Optional: Add styles for the gallery
+
+function Gallery({ tours, onRemove }) {
+  return (
+    <div className="gallery">
+      {tours.map((tour) => (
+        <TourCard
+          key={tour.id}
+          id={tour.id}
+          name={tour.name}
+          info={tour.info}
+          image={tour.image}
+          price={tour.price}
+          onRemove={onRemove}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Gallery;
